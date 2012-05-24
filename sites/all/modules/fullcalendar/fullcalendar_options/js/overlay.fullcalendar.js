ff91baf191309076121304074b075bdf5877826c
Campbell Vertesi <campbell@vertesi.com>
2012-05-24 18:31:54 +0200

    refs #366 - patching fullcalendar for Overlay support

---
+++ sites/all/modules/fullcalendar/fullcalendar_options/js/overlay.fullcalendar.js
@@ -0,0 +1,28 @@
+(function($) {
+
+Drupal.fullcalendar.plugins.overlay = {
+  options: function (fullcalendar, settings) {
+    if (!settings.overlay) {
+      return;
+    }
+    settings = settings.overlay;
+
+    var options = {
+      eventClick: function(calEvent, jsEvent, view) {
+        // Use overlay only for events based on entities
+        if (calEvent.eid !== undefined) {
+          alert('penis');
+          var url = calEvent.url;
+          url = '#overlay=' + url;
+          $.overlay({
+            href: url
+          });
+        }
+        return false;
+      }
+    };
+    return options;
+  }
+};
+
+}(jQuery));
