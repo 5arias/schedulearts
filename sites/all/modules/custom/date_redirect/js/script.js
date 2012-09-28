56773e8dc22f26313ce999b78ba175623c50cda4
gergelycsonka <gergely.csonka@gmail.com>
2012-09-28 17:01:16 +0200

    code cleanup

--- sites/all/modules/custom/date_redirect/js/script.js
+++ sites/all/modules/custom/date_redirect/js/script.js
@@ -1,52 +1,44 @@
-jQuery(document).ready(function($) {
-  // Code that uses jQuery's $ can follow here.
-  $("#secondary-menu-links").append("<div id='selected'></div>");
-});
 
 /**
  * @file
  * A JavaScript file for the theme.
- *
- * In order for this JavaScript to be loaded on pages, see the instructions in
- * the README.txt next to this file.
  */
 
+jQuery(document).ready(function($) {
+  $("#block-system-user-menu").append('<div id="hidden-field" style="display"></div>');
+});
 
-// JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it with an "anonymous closure". See:
-// - http://drupal.org/node/1446420
-// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
- Drupal.behaviors.DateRedirect = {
-      attach:
-          function(context, settings) {
-               var $dp = $("<input type='text' />").hide().datepicker({
-                 onSelect: function(dateText, inst) {
-                   $('#selected').text(dateText);
-                   var l = window.location;
-                   var base_url = l.protocol + "//" + l.host + "/" + l.pathname.split('/')[1];
-                   window.location.replace(base_url + "/print/schedule/" + dateText + "/print");
-                 }
-               }).appendTo('#secondary-menu-links');
+  Drupal.behaviors.DateRedirect = {
+    attach:
+      function(context, settings) {
+        $('a[title="Date popup redirect"]').addClass('date-select');
+        var $dp = $('<input type="text" />').hide().datepicker({
+          onSelect: function(dateText, inst) {
+            $('#hidden-field').text(dateText);
+            window.location.replace(Drupal.settings.date_redirect.base_url + '/print/schedule/' + dateText + '/print');
+          }
+        }).appendTo('#block-system-user-menu');
 
-               $(".date_select").click(function(e) {
-               e.preventDefault();
-               if ($dp.datepicker('widget').is(':hidden')) {
-                  $dp.datepicker( "option", "dateFormat", 'yy-mm-dd' );
-                  $dp.datepicker("show");
-                  $dp.datepicker("widget").position({
-                    my: "right top",
-                    at: "right top",
-                    of: this
-                  });
-                  
-              } else {
-                $dp.hide();
-              }
+        $("a.date-select").click(function(e) {
+        e.preventDefault();
+        if ($dp.datepicker('widget').is(':hidden')) {
+          $dp.datepicker('option', 'dateFormat', 'yy-mm-dd');
+          $dp.datepicker('show');
+          $dp.datepicker("widget").position({
+            my: 'right top',
+            at: 'right top',
+            of: this
+          });
 
-             });
- 
         }
-     }
+        else {
+          $dp.hide();
+        }
+
+      });
+ 
+    }
+  }
 })(jQuery, Drupal, this, this.document);
 
