eb46f360a21f069237b0e0ed4f056556cb6ad43d
Campbell Vertesi <campbell@vertesi.com>
2013-03-11 14:42:05 +0100

    refs #475 code updates

--- sites/all/modules/entityreference_browser/entityreference_browser.js
+++ sites/all/modules/entityreference_browser/entityreference_browser.js
@@ -1,5 +1,4 @@
 (function($) {
-
   Drupal.behaviors.entityreferenceBrowser = {
     attach: function (context, settings) {
       // Get the params from the hash.
@@ -12,99 +11,119 @@
           data[p[0]] = data[p[0]]?((data[p[0]] instanceof Array)?(data[p[0]].push(p[1]),data[p[0]]):[data[p[0]],p[1]]):p[1];
         });
       }
-    }
-  };
 
-  $(document).ready(function() {
-    var data = {};
-    // Add view settings to the data.
-    for (var key in Drupal.settings.views.ajaxViews[0]) {
-      data[key] = Drupal.settings.views.ajaxViews[0][key];
-    }
+    $('.entityreference-browser-wrapper', context).once('entityreference-browser-view-added', function() {
+      var element = $(this),
+        viewInfo = $.grep(settings.views.ajaxViews, function(e) {
+          return e.entityreference_browser_id == element.attr('id');
+        })[0],
+        target = $('div.entityreference-browser-view-widget', element);
 
-    $.ajax({
-      url: Drupal.settings.views.ajax_path,
-      type: 'GET',
-      data: data,
-      success: function(response) {
-        var viewDiv = '.view-dom-id-' + data.view_dom_id;
-        var target = $('div.entityreference-browser-view-widget');
-        if (response[2] != null) {
-          target.html(response[2].data);
-        }
-        if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
-          $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
-            // @todo: Figure out where to store the object.
-            new Drupal.views.ajaxView(settings);
-          });
-        }
-
-        // put a listener on the drag / drop UL item
-        var $select = $(".entityreference-browser-selected");
-        $select.sortable({
-          update: entityreference_browser_update
+        $.ajax({
+          url: settings.views.ajax_path,
+          type: 'GET',
+          data: viewInfo,
+          success: function(response) {
+            if (response[2] !== null) {
+              target.html(response[2].data);
+            }
+            // put a listener on the drag / drop UL item
+            $('.entityreference-browser-selected', element).sortable({
+              update: Drupal.behaviors.entityreferenceBrowser.browserUpdate
+            });
+            // This init call causes duplicate entries in the picker list.
+            //Drupal.behaviors.entityreferenceBrowser.init(element);
+            Drupal.attachBehaviors(target);
+          },
+          error: function(xhr) {
+            // Show an error to the user.
+            target.html('<p>The view didn\'t load properly. Please refresh page to try again.</p>');
+          },
+          dataType: 'json'
         });
-      },
-      error: function(xhr) {
-        // Show an error to the user.
-        var target = $('div.entityreference-browser-view-widget');
-        target.html('<p>The view didn\'t load properly. Please refresh page to try again.</p>');
-      },
-      dataType: 'json'
     });
 
-    $('body').delegate('a.picker', 'click', function(event) {
+    $('body', context).delegate('a.picker', 'click', function(event) {
       event.preventDefault;
 
-      var entityId = $(this).attr('data-id');
-      var label = $(this).text();
-      
-      // Check cardinality to see if user is allowed to add any more items 
+      var instance = $(this).closest('.entityreference-browser-wrapper'),
+        instanceId = instance.attr('id'),
+        instanceSettings = settings.entityreference_browser[instanceId],
+        entityId = $(this).attr('data-id');
+
+      // Check cardinality to see if user is allowed to add any more items
       // if it is -1 then the user is allowed to add unlimited items
-      var cardinality = Drupal.settings.entityreference_browser.cardinality;
-      if (cardinality > 0) {
+      if (instanceSettings.cardinality > 0) {
         // Get the count of the current selected items.
-        var selectedItemCount = $('ul.entityreference-browser li').length;
-        if (selectedItemCount >= cardinality) {
+        var selectedItemCount = $('ul.entityreference-browser li', instance).length;
+        if (selectedItemCount >= instanceSettings.cardinality) {
           // Don't allow the user to add any more items.
           return false;
         }
       }
 
       // Add entity only one time.
-      if(!$('li[data-id="' + entityId + '"]').length) {
-        // We should support more than only nodes.
-        var entityItemRemove = $('<a />').attr('href', Drupal.settings.basePath + 'node/' + entityId).attr('class', 'remove').attr('data-id', entityId).html('[' + Drupal.t('remove') + ']');
-        var entityItem = $('<li />').attr('data-id', entityId).html(label + ' ').append(entityItemRemove);
-
-        $('ul.entityreference-browser').append(entityItem);
-
-        entityreference_browser_update();
+      if(!$('li[data-id="' + entityId + '"]', instance).length) {
+        Drupal.behaviors.entityreferenceBrowser.addItem(entityId, instance);
       }
 
       return false;
     });
 
-    $('body').delegate('a.remove', 'click', function(event) {
+    $('body', context).delegate('a.remove', 'click', function(event) {
       event.preventDefault;
 
-      var entityId = $(this).attr('data-id');
+      var element = $(this).closest('div.entityreference-browser-wrapper'),
+        entityId = $(this).attr('data-id'),
+        item = $('li[data-id="' + entityId + '"]'),
+        list = item.closest('[id^=entityreference-browser]');
 
-      $('li[data-id="' + entityId + '"]').remove();
+      Drupal.detachBehaviors(item);
+      item.remove();
+      Drupal.behaviors.entityreferenceBrowser.browserUpdate('', {item: list});
 
-      entityreference_browser_update();
       return false;
     });
+    },
 
-  });
+    init: function(element) {
+      // Initialize the browser once.
+      $('ul.entityreference-browser-selected', element).once('entityreference-browser-selected', function() {
+        // @TODO use the proper field-type- class. See browserUpdate.
+        var field = element.closest('.field-type-entityreference'),
+          hidden = field.find('input.entityreference-browser-values'),
+          values = hidden.val();
 
-  function entityreference_browser_update(event, ui) {
-    var items = [];
-    $(".entityreference-browser-selected li").each(function(index) {
-      items.push($(this).attr('data-id'));
-    });
+        if (values !== '') {
+          values = values.split(',');
+          $.each(values, function(index, value) {
+                  Drupal.behaviors.entityreferenceBrowser.addItem(value, element);
+          });
+        }
+      });
+    },
 
-    $("input.entityreference-browser-values").val(items.join(','));
-  }
+    addItem: function(entityId, instance) {
+      var label = $('a[data-id="' + entityId + '"]').text(),
+        entityItemRemove = $('<a />').attr('href', Drupal.settings.basePath + 'node/' + entityId).attr('class', 'remove').attr('data-id', entityId).html('[' + Drupal.t('remove') + ']'),
+        entityItem = $('<li />').attr('data-id', entityId).html(label + ' ').append(entityItemRemove);
 
+      $('ul.entityreference-browser', instance).append(entityItem);
+      Drupal.behaviors.entityreferenceBrowser.browserUpdate('', {item: instance});
+      Drupal.attachBehaviors(instance);
+    },
+
+    browserUpdate: function (event, ui) {
+      var items = [],
+      instanceId = ui.item.attr('id'),
+      instanceSettings = Drupal.settings.entityreference_browser[instanceId];
+      field = ui.item.closest('.field-type-' + instanceSettings.field_type);
+
+      $(".entityreference-browser-selected li", field).each(function(index) {
+              items.push($(this).attr('data-id'));
+      });
+
+      $("input.entityreference-browser-values", field).val(items.join(','));
+     }
+  };
 })(jQuery);
