b91582d095b633c3b97fad5652ba87d9cc9506ea
Mau Palantír <mau.palantir@gmail.com>
2013-04-23 13:35:50 +0200

    refs #459 add schedulearts_navigator.js for proper overlays

---
+++ sites/all/modules/custom/schedulearts/schedulearts_navigator.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Process Navigator links to display in Overlay properly.
+ */
+(function($){
+  $(document).ready(function() {
+    $.each($('a.fc-event'), function(i, event_link) {
+      var href= $(event_link).attr('href');  
+      if (typeof(href.decodeURI) === 'function') {
+        $(event_link).attr('href', href.decodeURI());
+      }
+    });
+  });
+)(jQuery); //
