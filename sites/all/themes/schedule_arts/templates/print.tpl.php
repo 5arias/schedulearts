5812d8fe2727753e15b3fa6923f8a8259dc1ce01
Kovács Csaba <csaba.kovacs@cheppers.com>
2013-06-06 10:57:44 +0200

    Refs #555 - Theme schedule print format

--- sites/all/themes/schedule_arts/templates/print.tpl.php
+++ sites/all/themes/schedule_arts/templates/print.tpl.php
@@ -6,13 +6,12 @@
  *
  * @ingroup print
  */
-
+  global $base_url;
   $print['site_name'] = variable_get('site_name', 'Drupal');
   
   $path = explode("/", current_path());
   $timestamp = strtotime($path['1']);
-  $print['date'] = "Schedule for " . date('l F j, Y', $timestamp);  
-
+  $custom_css = $base_url . '/' . drupal_get_path('theme', 'schedule_arts') . '/css/print.css';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $print['language']; ?>" xml:lang="<?php print $print['language']; ?>">
@@ -25,10 +24,12 @@
     <?php print $print['robots_meta']; ?>
     <?php print $print['favicon']; ?>
     <?php print $print['css']; ?>
+    <style type="text/css" media="all">
+      @import url("<?php print $custom_css; ?>");
+    </style>
   </head>
   <body class="schedulearts-print">
     <div class="print-site_name"><?php print $print['site_name']; ?></div>
-    <div class="print-date"><?php print $print['date']; ?></div>
     <p />
     <div class="print-content"><?php print $print['content']; ?></div>
   </body>
