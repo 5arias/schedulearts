7a46732d4ca9bd0b8221bc88ada05d1d670b25b1
Akos Toth <akos.toth@cheppers.com>
2013-07-09 15:10:27 +0200

    let's try a hack then

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,10 +1,20 @@
 function replaceOverlay(date) {
-  jQuery(document).bind('drupalOverlayBeforeClose', function() {
+  /*jQuery(document).bind('drupalOverlayBeforeClose', function() {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
     }
     else {
       window.location.href =  window.location.href + '&date=' + date;
     }
-  });
+  });*/
+  // Hack.
+  Drupal.overlay.eventhandlerRefreshPage = function (event) {
+    alert('evhandler running');
+    if (window.location.href.indexOf('?', 0) === -1) {
+      window.location.href =  window.location.href + '?date=' + date;
+    }
+    else {
+      window.location.href =  window.location.href + '&date=' + date;
+    }
+  };
 }
