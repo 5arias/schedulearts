6cf66e0852562f213dcacc48bff236055eb78d94
Akos Toth <akos.toth@cheppers.com>
2013-07-09 16:37:29 +0200

    testing with live

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,5 +1,5 @@
 function replaceOverlay(date) {
-  jQuery(document).bind('drupalOverlayBeforeClose', function() {
+  jQuery(document).live('drupalOverlayBeforeClose', function() {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
     }
