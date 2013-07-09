520aaba26621838af71a136b1c55daf1850105f5
Akos Toth <akos.toth@cheppers.com>
2013-07-09 14:55:41 +0200

    harder unbind, may have implications

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,6 +1,6 @@
 function replaceOverlay(date) {
   var eventClass = '.drupal-overlay.drupal-overlay-open';
-  jQuery(window).unbind('drupalOverlayClose' + eventClass, jQuery.proxy(Drupal.overlay, 'eventhandlerRefreshPage'));
+  jQuery(window).unbind('drupalOverlayClose' + eventClass);
   jQuery(window).bind('drupalOverlayClose' + eventClass, function() {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
