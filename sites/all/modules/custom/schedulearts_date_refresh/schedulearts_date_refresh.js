14d58caec801c69f79898de77720eb8d7c8e64b7
Akos Toth <akos.toth@cheppers.com>
2013-07-09 16:26:48 +0200

    fine

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,20 +1,10 @@
 function replaceOverlay(date) {
-  /*jQuery(document).bind('drupalOverlayBeforeClose', function() {
+  jQuery(document).bind('drupalOverlayBeforeClose', function() {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
     }
     else {
       window.location.href =  window.location.href + '&date=' + date;
     }
-  });*/
-  // Hack.
-  Drupal.overlay.eventhandlerRefreshPage = function (event) {
-    alert('evhandler running');
-    if (window.location.href.indexOf('?', 0) === -1) {
-      window.location.href =  window.location.href + '?date=' + date;
-    }
-    else {
-      window.location.href =  window.location.href + '&date=' + date;
-    }
-  };
+  });
 }
