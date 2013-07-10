bb2bc4af87354b1d5ecfd3fab5934a95dee572b6
Akos Toth <akos.toth@cheppers.com>
2013-07-10 14:35:27 +0200

    getting a lot warmer

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,19 +1,10 @@
 function replaceOverlay(date) {
-  window.top.Drupal.overlay.eventHandlerRefreshPage = function (event) {
+  jQuery(window.top.document).bind('drupalOverlayBeforeClose', function() {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
     }
     else {
       window.location.href =  window.location.href + '&date=' + date;
     }
-  };
-  /*jQuery(window.top.document).live('drupalOverlayBeforeClose', function() {
-    if (window.location.href.indexOf('?', 0) === -1) {
-      window.location.href =  window.location.href + '?date=' + date;
-    }
-    else {
-      window.location.href =  window.location.href + '&date=' + date;
-    }
-  });*/
-  //Drupal.overlay.isOpen; // This will generate a JS breakpoint that might be useful.
+  });
 }
