e29cd86f497e607e3cd331b7ff4a7c0ade0bcf27
Akos Toth <akos.toth@cheppers.com>
2013-07-10 14:30:00 +0200

    handler replace tset

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,11 +1,19 @@
 function replaceOverlay(date) {
-  jQuery(window.top.document).live('drupalOverlayBeforeClose', function() {
+  window.top.Drupal.overlay.eventHandlerRefreshPage = function (event) {
     if (window.location.href.indexOf('?', 0) === -1) {
       window.location.href =  window.location.href + '?date=' + date;
     }
     else {
       window.location.href =  window.location.href + '&date=' + date;
     }
-  });
+  };
+  /*jQuery(window.top.document).live('drupalOverlayBeforeClose', function() {
+    if (window.location.href.indexOf('?', 0) === -1) {
+      window.location.href =  window.location.href + '?date=' + date;
+    }
+    else {
+      window.location.href =  window.location.href + '&date=' + date;
+    }
+  });*/
   //Drupal.overlay.isOpen; // This will generate a JS breakpoint that might be useful.
 }
