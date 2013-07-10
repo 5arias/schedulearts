6b351eb3892647738fceda6dacadd35dffd00253
Akos Toth <akos.toth@cheppers.com>
2013-07-10 15:03:12 +0200

    hm.

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,14 +1,11 @@
-function addParamToURL(url, param) {
-  _url = url;
-  _url = (_url.indexOf("?") !== -1 ?
+function replaceOverlay(date) {
+  jQuery(window.top.document).bind('drupalOverlayBeforeClose', function() {
+    _url = window.top.location.href;
+    param = "date="+date
+    _url = (_url.indexOf("?") !== -1 ?
          _url.split("?")[0]+"?"+param+"&"+_url.split("?")[1] :
          (_url.indexOf("#") !== -1 ?
           _url.split("#")[0]+"?"+param+"#"+ _url.split("#")[1] : _url+'?'+param));
-  return _url;
-}
-
-function replaceOverlay(date) {
-  jQuery(window.top.document).bind('drupalOverlayBeforeClose', function() {
-    window.top.location.href = addParamToURL(window.top.location.href, "date="+date);
+    window.top.location.href = _url;
   });
 }
