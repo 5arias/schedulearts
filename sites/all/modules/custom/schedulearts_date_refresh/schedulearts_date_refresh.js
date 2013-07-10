ebafb9127c2924328c9fa8f62e8c7289c793de81
Akos Toth <akos.toth@cheppers.com>
2013-07-10 15:23:55 +0200

    okay now i'll make it uglier

--- sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
+++ sites/all/modules/custom/schedulearts_date_refresh/schedulearts_date_refresh.js
@@ -1,7 +1,7 @@
 function replaceOverlay(date) {
   jQuery(window.top.document).bind('drupalOverlayBeforeClose', function() {
     _url = window.top.location.href;
-    param = "date="+date
+    param = "date="+date;
     _url = (_url.indexOf("?") !== -1 ?
          _url.split("?")[0]+"?"+param+"&"+_url.split("?")[1] :
          (_url.indexOf("#") !== -1 ?
