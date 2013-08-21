0eff89ec598737f2a0705f545a18b90d8b4cb2ad
Mau Palantír <mau.palantir@gmail.com>
2013-08-21 15:35:59 +0200

    refs #567 added content blocks to rendered pages like in overlay.

--- sites/all/modules/custom/ctools_overlay/js/ctools_overlay.fullcalendar.js
+++ sites/all/modules/custom/ctools_overlay/js/ctools_overlay.fullcalendar.js
@@ -1,13 +1,12 @@
 (function($) {
-
+// Set ctools modal weight to process fullcalendar too.
+Drupal.behaviors["ZZCToolsModal.weight"] = 100;
 
 Drupal.fullcalendar.plugins.ctools_overlay = {
   options: function (fullcalendar, settings) {
     if (!settings.ctools_overlay) {
       return false;
     }
-    // Set ctools modal weight to process fullcalendar too.
-    Drupal.behaviors["ZZCToolsModal.weight"] = 100;
 
     settings = settings.ctools_overlay;
     var options = {
